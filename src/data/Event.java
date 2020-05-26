c79ca7708616358f6debad842438165c04f3a644
MikeBrownse <2534701930@qq.com>
2020-05-26 22:33:00 +0800

    Attention: Month in "Calendar" starts form 0, ends to 11.

--- src/data/Event.java
+++ src/data/Event.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class Event {
@@ -37,7 +39,7 @@ public class Event {
 	public void setDetials(String newDetials) {
 		detials = newDetials;
 	}
-	
+	//设置时间//注意calendar类的初始月份为0
 	public void setDeadline(Calendar newDeadline) {
 		deadline = newDeadline;
 	}
@@ -67,10 +69,6 @@ public class Event {
 		return deadline;
 	}
 	
-	public String getEventString(Event e) {
-		return null;
-	}
-	
 	public void getEventInfo(Event e) {
 		System.out.println(e.getEventID() + " "
 				+ e.getTitle() + " "
@@ -81,10 +79,9 @@ public class Event {
 	}
 	//时间
 	public String getStringTime(Calendar c) {
-//		return String.format("%04d年%02d月%02日 %02d:%02d", c.get(c.YEAR), c.get(c.MONTH), c.get(c.DAY_OF_MONTH),
-//				c.get(c.HOUR_OF_DAY), c.get(c.MINUTE));
-		return (c.get(c.YEAR) + "年" + c.get(c.MONTH) + "月" + c.get(c.DAY_OF_MONTH) + "日 "
-				+ c.get(c.HOUR_OF_DAY) + ":" + c.get(c.MINUTE));
+		Date d = c.getTime();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日  HH:mm");
+		return sdf.format(d);
 	}
 	public static void main(String[] args) {
 		// TODO 自动生成的方法存根
@@ -93,8 +90,8 @@ public class Event {
 //		System.out.println(eve.getTitle() + eve.getState());
 		
 		//日期生成、转换测试
-//		Calendar c = Calendar.getInstance();
-//		c.set(2020, 5, 25, 23, 8);
+		Calendar c = Calendar.getInstance();
+		c.set(2020, 5 - 1, 25, 23, 8);//calendar类MONTH的初值为0
 //		System.out.println(c.get(c.YEAR) + "-" + c.get(c.MONTH) + "-" + c.get(c.DAY_OF_MONTH) + " "
 //				+ c.get(c.HOUR_OF_DAY) + ":" + c.get(c.MINUTE) + ":" + c.get(c.SECOND));
 //		Date d = c.getTime();
@@ -102,13 +99,15 @@ public class Event {
 //		c1.setTime(d);
 //		System.out.println(c1.get(c.YEAR) + "-" + c1.get(c.MONTH) + "-" + c1.get(c.DAY_OF_MONTH) + " "
 //				+ c1.get(c.HOUR_OF_DAY) + ":" + c1.get(c.MINUTE) + ":" + c1.get(c.SECOND));
+		Event e = new Event(0, null, null, null, c, null);
+		System.out.println(e.getStringTime(c));
 		
 		//event + calendar test
-		Calendar ddl = Calendar.getInstance();
-		Calendar ddl1 = Calendar.getInstance();
-		ddl.set(2020, 5, 30, 8, 10);
-		ddl1.set(2020, 5, 26, 17, 18);
-		System.out.println(ddl.compareTo(ddl1));
+//		Calendar ddl = Calendar.getInstance();
+//		Calendar ddl1 = Calendar.getInstance();
+//		ddl.set(2020, 5, 30, 8, 10);
+//		ddl1.set(2020, 5, 26, 17, 18);
+//		System.out.println(ddl.compareTo(ddl1));
 //		Event e = new Event(1, "hello", 1, 1, ddl, null);
 //		System.out.println(e.getStringTime(e.deadline));
 	}
